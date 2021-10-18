2f2d42918939e68e25bcd2a0b465512b9acc2315
MarcoNaik <marcogomezf@gmail.com>
2021-10-18 18:33:44 -0300

    refactor(constants): added https schema and switched to sandbox url

--- lib/plutto/constants.rb
+++ lib/plutto/constants.rb
@@ -1,6 +1,7 @@
 module Plutto
   module Constants
     GENERAL_DOC_URL = 'https://docs.getplutto.com/'
-    BASE_URL = 'app.getplutto.com/api/v1/'
+    SCHEME = 'https://'
+    BASE_URL = 'sandbox.getplutto.com/api/v1/'
   end
 end
