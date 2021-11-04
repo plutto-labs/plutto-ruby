62a5fdb964e931022320fe9d3f5b139c96df7fed
MarcoNaik <marcogomezf@gmail.com>
2021-11-04 09:12:45 -0300

    test(resources/subscription): subscription object methods test

--- spec/lib/plutto/resource/subscription_spec.rb
+++ spec/lib/plutto/resource/subscription_spec.rb
@@ -2,6 +2,8 @@ require 'plutto/resources/subscription'
 require 'plutto/resources/pricing'
 
 RSpec.describe Plutto::Subscription do
+  let(:api_key) { 'sk_live_0a641854b17cdfed9e98eb7cd3e9e2bfc2687a37470cc8ce71f31c49e33d037f' }
+  let(:client) { Plutto::Client.new(api_key) }
   let(:data) do
     {
       id: "subscription_1cc5fa39987c48d4dde5c122",
@@ -12,29 +14,30 @@ RSpec.describe Plutto::Subscription do
       trial_finishes_at: nil,
       bills_at: nil,
       billing_period_duration: "P1M",
-      pricings: [
-        {
-          id: "pricing_ea1f7c8c15bee31736d12242",
-          name: "Básico",
-          currency: "CLP",
-          product_id: "product_cb870654623ffa74dc26c647",
-          price_logics: [
-            {
-              id: "price-logic_0ce38b40f4c1752412ac3b72",
-              type: "FlatFee",
-              price: "15000.0",
-              price_currency: "CLP",
-              meter_count_method: nil,
-              tiers: nil
-            }
-          ]
-        }
-      ],
+      pricings: [],
       permission_group: nil
     }
   end
 
-  let(:subscription) { described_class.new(**data) }
+  let(:subscription) { described_class.new(**data, client: client) }
+  let(:pricing) do
+    {
+      id: "pricing_ea1f7c8c15bee31736d12242",
+      name: "Básico",
+      currency: "CLP",
+      product_id: "product_cb870654623ffa74dc26c647",
+      price_logics: [
+        {
+          id: "price-logic_0ce38b40f4c1752412ac3b72",
+          type: "FlatFee",
+          price: "15000.0",
+          price_currency: "CLP",
+          meter_count_method: nil,
+          tiers: nil
+        }
+      ]
+    }
+  end
 
   it 'create an instance of Subscription' do
     expect(subscription).to be_an_instance_of(described_class)
@@ -45,5 +48,37 @@ RSpec.describe Plutto::Subscription do
     expect(subscription.to_s).to eq(
       "customer_e9585b900c38db249f38f59c is subscribed since 20 Oct 2021"
     )
+  end
+
+  describe '#end' do
+    it 'calls end_subscription on client', :vcr do
+      allow(client).to receive(:end_subscription).with(subscription_id: subscription.id)
+      subscription.end
+      expect(client).to have_received(:end_subscription).with(subscription_id: subscription.id).once
+    end
+  end
+
+  describe '#add_pricings' do
+    it 'calls add_pricings on client', :vcr do
+      allow(client).to receive(:add_pricings).with(
+        subscription_id: subscription.id, pricing_ids: [pricing[:id]]
+      )
+      subscription.add_pricings(pricing[:id])
+      expect(client).to have_received(:add_pricings).with(
+        subscription_id: subscription.id, pricing_ids: [pricing[:id]]
+      ).once
+    end
+  end
+
+  describe '#remove_pricings' do
+    it 'calls remove_pricings on client', :vcr do
+      allow(client).to receive(:remove_pricings).with(
+        subscription_id: subscription.id, pricing_ids: [pricing[:id]]
+      )
+      subscription.remove_pricings(pricing[:id])
+      expect(client).to have_received(:remove_pricings).with(
+        subscription_id: subscription.id, pricing_ids: [pricing[:id]]
+      ).once
+    end
   end
 end
